e11980095bcff1220281d78d2c5b5712157fd755
aquinnmo <aquinnmo@outlook.com>
2026-04-23 15:54:45 -0400

    first commit

---
+++ src/main/kotlin/ca/adam_montgomery/moneyball/MoneyballApplication.kt
@@ -0,0 +1,11 @@
+package ca.adam_montgomery.moneyball
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoneyballApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoneyballApplication>(*args)
+}
---
+++ src/test/kotlin/ca/adam_montgomery/moneyball/MoneyballApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.adam_montgomery.moneyball
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoneyballApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
